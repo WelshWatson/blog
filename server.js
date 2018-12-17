c2e3d7552b5ff1f1dbf8b7fe19071f0ae932f861
WelshWatson <owen_watson84@hotmail.com>
2018-12-17 23:40:26 +0000

    Moved assets and added final pages from nav bar

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require("body-parser");
 
 var app = express();
+
+app.use(bodyParser.json(), function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/the-watson'));
 
-app.get('/*', function(req,res) {
+app.get('*', function(req,res) {
 
 res.sendFile(path.join(__dirname+'/dist/the-watson/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+    console.log("App now running on port", port);
+});
