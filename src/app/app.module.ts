20ed008f18cdeeb37a8dbab1c4e30da2e725d087
WelshWatson <owen_watson84@hotmail.com>
2018-12-17 18:33:09 +0000

    Styles, components and routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './components/root/app.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {HomeComponent} from './components/home/home.component';
+import {SkillsComponent} from './components/skills/skills.component';
+import {AboutComponent} from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SkillsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
